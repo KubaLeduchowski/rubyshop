01cb5e32ab833d720c46e6cf1ba79b1ece0a9920
kubaleduchowski <leduchowskijakub@gmail.com>
2018-02-15 15:40:27 +0100

    sending confirmation email

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+Depot::Application.configure do
+config.action_mailer.delivery_method = :smtp
+end
