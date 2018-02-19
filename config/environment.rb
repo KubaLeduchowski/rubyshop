40f46d07e401a56974bc5ff3e642c0760d3c1130
kubaleduchowski <leduchowskijakub@gmail.com>
2018-02-19 13:36:06 +0100

    create user

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-Depot::Application.configure do
-config.action_mailer.delivery_method = :smtp
-end
